81da8072239c407125039507cce73c697bddb010
Gabriele Tronchin <gabriele.tronchin@tecalliance.net>
2025-03-15 15:41:27 +0000

    Adjust launch settings; Add swagger;

--- OptimisticConcurrencyWithEFCore/Program.cs
+++ OptimisticConcurrencyWithEFCore/Program.cs
@@ -3,12 +3,16 @@ using OptimisticConcurrency.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 builder.Services.AddPersistence(builder.Configuration);
 
 var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
